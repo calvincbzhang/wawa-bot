600de8a64ea96c6d57b85c4d2168be01c6d57e81
Ivan <ivan.dewerpe@student.manchester.ac.uk>
2020-03-25 19:38:51 +0100

    Complete localhost basic version of chatbot

--- WawaBot/app.py
+++ WawaBot/app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  return "Hello World!"
+def home_reply():
+  return "Hello World"
+
+@app.route("/sms", methods=['POST'])
+def whatsapp_reply():
+  """
+    Respond to basic message on whatsapp
+  """
+  msg = request.form.get('body')
+
+  #Start out response
+  resp = MessagingResponse()
+
+  #Add a message
+  resp.message("")
+
+  return str(resp)
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
--- WawaBot/bot.py
+++ WawaBot/bot.py
@@ -4,3 +4,10 @@ account_sid = os.environ['TWILIO_ACCOUNT_SID']
 account_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, account_token)
 
+message = client.messages.create(
+                            body='Hello There',
+                            from_='whatsapp:+1415523886'
+                            to='whatsapp:+447708438644'
+                        )
+
+print(message.sid)
