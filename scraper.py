41c63418287b2093d011198e60252478bd957219
Chen Bo Calvin Zhang <chen.zhang-10@student.manchester.ac.uk>
2020-03-25 16:48:26 +0000

    Scraper for memes from Reddit

---
+++ scraper.py
@@ -0,0 +1,61 @@
+import io
+from PIL import Image
+import os
+import hashlib
+import requests
+import csv
+import time
+from bs4 import BeautifulSoup as soup
+
+def persist_image(folder_path:str, url:str):
+  try:
+    image_content = requests.get(url).content
+
+  except Exception as e:
+    print(f"ERROR - Could not download {url} - {e}")
+
+  try:
+    image_file = io.BytesIO(image_content)
+    image = Image.open(image_file).convert('RGB')
+    file_path = os.path.join(folder_path, hashlib.sha1(image_content).hexdigest()[:10] + '.jpg')
+    with open(file_path, 'wb') as f:
+      image.save(f, "JPEG", quality=85)
+    print(f"SUCCESS - saved {url} - as {file_path}")
+  except Exception as e:
+    print(f"ERROR - Could not save {url} - {e}")
+
+
+
+url = "https://old.reddit.com/r/Coronavirus_Meme/"
+# Headers to mimic a browser visit
+headers = {'User-Agent': 'Mozilla/5.0'}
+
+# Returns a requests.models.Response object
+page = requests.get(url, headers=headers)
+
+soup_page = soup(page.text, 'html.parser')
+
+attrs = {'class': 'thing', 'data-domain': 'i.redd.it'}
+
+counter = 1
+
+while (counter <= 30):
+  posts = soup_page.find_all('div', attrs=attrs)
+  for post in posts:
+    image = post.find("a", class_="thumbnail")
+    image_page_link = 'http://old.reddit.com' + image.attrs['href'] + '?'
+    image_page = requests.get(image_page_link, headers=headers)
+    image_soup_page = soup(image_page.text, 'html.parser')
+    
+    file = image_soup_page.find("img", class_="preview")
+    file_link = file.attrs['src']
+    
+    persist_image('./images', file_link)
+
+    counter += 1
+
+  next_button = soup_page.find("span", class_="next-button")
+  next_page_link = next_button.find("a").attrs['href']
+  time.sleep(2)
+  page = requests.get(next_page_link, headers=headers)
+  soup_page = soup(page.text, 'html.parser')
